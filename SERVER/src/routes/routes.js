98910d6ea06730f5369d8136b3f5ff2fc729958c
Fabiana1vo <fabianasouzaivo@icloud.com>
2023-04-23 20:22:24 -0300

    primeiro commit

---
+++ SERVER/src/routes/routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const promptController = require('../controllers/prompt-controller')
+const routes = express.Router()
+
+routes.post('/api/prompt', promptController.sendText)
+
+module.exports = routes
+
+
+
+// http://localhost:5009/api/prompt
